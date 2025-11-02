94eb912cc6fc14a963c4c1a02cc8ec9d9c921c8b
YuriZem <yurizem333@gmail.com>
2025-11-02 16:41:01 -0300

    mais um livro

--- data/books.js
+++ data/books.js
@@ -1,5 +1,6 @@
 [
   { "id": 1, "title": "O Hobbit", "author": "J.R.R. Tolkien", "year": 1937 },
   { "id": 2, "title": "1984", "author": "George Orwell", "year": 1949 },
-  { "id": 3, "title": "Dom Casmurro", "author": "Machado de Assis", "year": 1899 }
+  { "id": 3, "title": "Dom Casmurro", "author": "Machado de Assis", "year": 1899 },
+  { "id": 4, "title": "Dom Casmurro", "author": "Machado de Assis", "year": 1899 }
 ]
