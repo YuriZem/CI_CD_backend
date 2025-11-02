762cef6286e854c8c1a8332c66db4cc20b9c27cf
YuriZem <yurizem333@gmail.com>
2025-11-02 16:52:21 -0300

    ajuste de rota

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const booksRoutes = require('./routes/books');
+const booksRoutes = require('routes/books');
 
 app.use(express.json());
 app.use('/books', booksRoutes);
